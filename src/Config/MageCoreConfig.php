a880b4f4b6649961fc072f86edb774c506ecb851
Justin Beaty <justin.beaty@gmail.com>
2024-12-09 06:26:20 -0800

    updates

--- src/Config/MageCoreConfig.php
+++ src/Config/MageCoreConfig.php
@@ -27,17 +27,26 @@ final class MageCoreConfig
         switch ("$class::$method") {
         case 'Mage::getModel':
         case 'Mage::getSingleton':
+        case 'Mage_Core_Model_Config::getModelInstance':
             return fn (string $alias) => $this->getConfig()->getModelClassName($alias);
         case 'Mage::getResourceModel':
         case 'Mage::getResourceSingleton':
+        case 'Mage_Core_Model_Config::getResourceModelInstance':
             return fn (string $alias) => $this->getConfig()->getResourceModelClassName($alias);
+        case 'Mage::getResourceHelper':
+        case 'Mage_Core_Model_Config::getResourceHelper':
+        case 'Mage_Core_Model_Config::getResourceHelperInstance':
+            return fn (string $alias) => $this->getConfig()->getResourceHelperClassName($alias);
         case 'Mage_Core_Model_Layout::createBlock':
         case 'Mage_Core_Model_Layout::getBlockSingleton':
             return fn (string $alias) => $this->getConfig()->getBlockClassName($alias);
         case 'Mage::helper':
         case 'Mage_Core_Model_Layout::helper':
         case 'Mage_Core_Block_Abstract::helper':
+        case 'Mage_Core_Model_Config::getHelperInstance':
             return fn (string $alias) => $this->getConfig()->getHelperClassName($alias);
+        case 'Mage_Core_Model_Config::getNodeClassInstance':
+            return fn (string $path) => $this->getConfig()->getNodeClassName($path);
         case 'Mage_Admin_Model_User::_helper':
         case 'Mage_Adminhtml_Controller_Rss_Abstract::_helper':
         case 'Mage_Api_Model_User::_helper':
